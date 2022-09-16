d962052312e34aac39eb2b8767d9ca87f1e02904
Jacob White <jake@jwhite.network>
2022-09-16 17:58:13 -0400

    Fork the code and make updates

--- src/routes.ts
+++ src/routes.ts
@@ -64,7 +64,7 @@ router.post("/upload", authMiddleware, async (request: Request, env: Env): Promi
 	}catch(error){
 		return new Response(JSON.stringify({
 			success: false,
-			message: "Error occured writing to R2",
+			message: "Error occurred writing to R2",
 			error: {
 				name: error.name,
 				message: error.message,
